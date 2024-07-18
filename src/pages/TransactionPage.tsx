2bac450e34d28c4d8ccebf4a0484190e5c3a20f3
Abhishek Singh <singh.abhishek@binmile.com>
2024-07-18 13:10:19 +0530

    upated home page

--- src/pages/TransactionPage.tsx
+++ src/pages/TransactionPage.tsx
@@ -14,43 +14,38 @@ import { useForm } from 'react-hook-form';
 import { Box, Card, CardContent, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Layout from '../components/Layout';
-
 const Container = styled(Box)({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: '20px 0',
-    padding: '20px',
+    // marginTop: '5px',
+    // padding: '2px',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    minHeight: '100vh',
+    minHeight: '65vh',
     backgroundColor: 'rgba(255, 255, 255, 0.9)',
 });
-
 const ContentWrapper = styled(Box)({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
-    padding: '20px 0',
+    // padding: '10px 0',
 });
-
 const StyledCard = styled(Card)(({ theme }) => ({
     width: '90%',
     maxWidth: '600px',
-    margin: '10px 0',
+    //margin: '10px 0',
     backgroundColor: 'white',
     opacity: 0.9,
 }));
-
 const Title = styled(Typography)({
     textAlign: 'center',
-    margin: '10px 0',
+    margin: '3px ',
     color: 'black',
 });
-
 const TransactionHistory: React.FC = () => {
     const dispatch = useDispatch();
     const transactions = useSelector(
@@ -58,38 +53,30 @@ const TransactionHistory: React.FC = () => {
     );
     const currentUser = useSelector((state: RootState) => state.users.currentUser);
     const { control } = useForm();
-
     const [category, setCategory] = useState<string>('');
     const [type, setType] = useState<string>('');
     const [query, setQuery] = useState<string>('');
-
     useEffect(() => {
         const fetchData = async () => {
             const transactions = await loadTransactionsFromStorage();
             dispatch(setTransactions(transactions));
         };
-
         fetchData();
     }, [dispatch]);
-
     const handleCategoryChange = (event: SelectChangeEvent<string>) => {
         setCategory(event.target.value);
     };
-
     const handleTypeChange = (event: SelectChangeEvent<string>) => {
         setType(event.target.value);
         setCategory('');
     };
-
     const filteredTransactions = transactions.filter((transaction) => {
         const matchesType = type ? transaction.type === type : true;
         const matchesCategory = category ? transaction.category === category : true;
         const matchesQuery = transaction.category.toLowerCase().includes(query.toLowerCase());
         const matchesUser = transaction.userId === currentUser?.id;
-
         return matchesType && matchesCategory && matchesQuery && matchesUser;
     });
-
     return (
         <Layout>
             <Container>
@@ -116,5 +103,4 @@ const TransactionHistory: React.FC = () => {
         </Layout>
     );
 };
-
 export default TransactionHistory;
